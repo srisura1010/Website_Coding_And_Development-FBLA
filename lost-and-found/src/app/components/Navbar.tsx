2ed46f7809453c43961a3f2c74f8ead43aa543e5
Srivatsav Sura <srivatsav4ever@gmail.com>
2026-01-17 10:51:03 -0700

    Updated navbar.tsx

--- lost-and-found/src/app/components/Navbar.tsx
+++ lost-and-found/src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      <h2 className="logo">MySite</h2>
+      <h2 className="logo">Lost And Found</h2>
       <ul className="nav-links">
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
